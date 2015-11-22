da49f7c5cdac808be5e83528335858a2502cd1e5
shuming2 <zsmingwh@gmail.com>
2015-11-22 14:45:20 -0700

    add test file

---
+++ servertestcase.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#CMPUT 379 Fall 2015 Assignment-2
+#Test Case: 1
+make
+
+export PROCNANNYLOGS="./logfile.log"
+export PROCNANNYSERVERINFO="./serverinfo"
+
+
+./procnanny.server config &
+
+
+
+
